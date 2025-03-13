4b3e2762fbef1275617b6d7221c46d4ebbc37a1e
can <canongun90@gmail.com>
2025-03-13 14:32:52 -0700

    Initial Commit

---
+++ Dynamic Translator/Dynamic_TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_TranslatorApp.swift
+//  Dynamic Translator
+//
+//  Created by can on 13.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dynamic TranslatorTests/Dynamic_TranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_TranslatorTests.swift
+//  Dynamic TranslatorTests
+//
+//  Created by can on 13.03.25.
+//
+
+import Testing
+@testable import Dynamic_Translator
+
+struct Dynamic_TranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
